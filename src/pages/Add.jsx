7c5b9c0558f8a040ca4df1069e492bb8e2966235
Wuttha <Wuttha@webmail.npru.ac.th>
2025-06-23 17:14:02 +0700

    อัปเดตหน้า Restaurants เพิ่มการ์ดร้านอาหารด้วย DaisyUI

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, Component } from "react";
 import Navbar from "../component/nuvbar";
 
 const Add = () => {
@@ -6,14 +6,43 @@ const Add = () => {
     title: "",
     type: "",
     img: "",
-});
-const handleChange = (e)=> {
-    const { name ,value} = e.target;
-    setRestaurants({...restaurant,[name]: value});
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRestaurants({ ...restaurant, [name]: value });
+  };
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+      <div>
+        <h1 className="title justify-center text-3xl text-center m-5 p-5">
+          Grab Restaurant
+        </h1>
+      </div>
+      <div className="mb-5 flex justify-center items-center ">
+        <label className="input flex items-center gap-2 w-2xl">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" required placeholder="Search" />
+        </label>
+      </div>
+      <Resturant restaurants={Restaurants} />
+    </div>
+  );
 };
-return (
-    <div className="container mx-auto"
-    
-)
 
 export default Add;
